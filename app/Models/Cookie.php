07d294c390046cbc5692678b4718d8890d081b0e
ManasArs13 <manasyan.a.s@ya.ru>
2023-12-17 12:16:53 +0300

    Fix modles

--- app/Models/Cookie.php
+++ app/Models/Cookie.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cookie extends Model
 {
     use HasFactory;
+
+    public function response()
+    {
+        return $this->belongsTo(Response::class);
+    }
 }
